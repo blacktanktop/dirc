d493629cfd12817162a6ee4ce8f6128186de2759
blacktanktop <blacktanktopme@gmail.com>
2019-09-13 07:27:04 +0900

    fix a little

--- template/bin/bq/bq_table_name/load_data.sh
+++ template/bin/bq/bq_table_name/load_data.sh
@@ -6,7 +6,7 @@ input="$HOME/PATH_TO_PROJECT_DIR/bin/bq/bq_tabel_name"
 
 gcloud config configurations activate default
 
-bq load --source_format=CSV --skip_leading_rows=1 [PROJECT:DATASET.TABLE] ~/PATH_TO_PROJECT_DIR/results/all_bq_out.csv ~/PATH_TO_PROJECT_DIR/bq/schema.json
+bq load --source_format=CSV --skip_leading_rows=1 PROJECT:DATASET.TABLE ~/PATH_TO_PROJECT_DIR/results/all_bq_out.csv ~/PATH_TO_PROJECT_DIR/bq/schema.json
 
 # headerがある場合は--skip_leading_rows=1とする
 # bq load [option] [project:dataset.table] [data] [schema]
